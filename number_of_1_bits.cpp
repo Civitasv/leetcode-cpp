ed8eab9890d42ee9f22e7dd5d0aa7df6010a19fb
Civitasv <hscivitasv@gmail.com>
2022-04-28 09:53:42 +0800

    day thirtheen finished

---
+++ number_of_1_bits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int ans = 0;
+        while (n)
+        {
+            ans += n & 1;
+            n >>= 1;
+        }
+        return ans;
+    }
+};
