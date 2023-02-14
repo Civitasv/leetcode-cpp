abf70774c893959682b7f917830de33d7234e974
Civitasv <hscivitasv@gmail.com>
2023-02-14 16:16:34 +0800

    代码随想录 贪心初识

---
+++ questions/carl/贪心/376.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <algorithm>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int wiggleMaxLength(vector<int> &nums) {
+    int dp[nums.size()][2];
+    dp[0][0] = dp[0][1] = 1;
+
+    for (int i = 1; i < nums.size(); i++) {
+      dp[i][0] = dp[i][1] = 1;
+      for (int j = 0; j < i; j++) {
+        if (nums[j] > nums[i])
+          dp[i][0] = max(dp[i][0], dp[j][1] + 1);
+        if (nums[j] < nums[i])
+          dp[i][1] = max(dp[i][1], dp[j][0] + 1);
+      }
+    }
+
+    return max(dp[nums.size() - 1][0], dp[nums.size() - 1][1]);
+  }
+};
---
+++ questions/carl/贪心/455.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findContentChildren(vector<int> &g, vector<int> &s) {
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+    int count = 0;
+    int index = 0;
+
+    for (int i = 0; i < g.size(); i++) {
+      while (index < s.size() && s[index] < g[i]) {
+        index++;
+      }
+      if (index < s.size()) {
+        index++;
+        count++;
+      } else
+        break;
+    }
+
+    return count;
+  }
+};
