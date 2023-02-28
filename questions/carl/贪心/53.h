451b7ef04b04a2c8b328b68f83530b456a152489
Civitasv <hscivitasv@gmail.com>
2023-02-28 13:59:12 +0800

    代码随想录 贪心 第一周

---
+++ questions/carl/贪心/53.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int dp[nums.size()];
+    int result = nums[0];
+    dp[0] = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+      dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+      if (dp[i] > result)
+        result = dp[i];
+    }
+
+    return result;
+  }
+};
